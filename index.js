8b2b0adbe76fd70e89e12b7673ec5f92a44aceea
Jamie Masalinto <jmasalinto@gmail.com>
2021-04-10 17:52:13 -0400

    commit tree bd58e27db6feeaea415fac4cdca70375cf638703

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31;
+const num2 = 2;
+const num3 = 14;
+const num4 = 5;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random());
+
+const mod = num3 % num4;
+
+const max = Math.max(1, 4, 8, 5, 20, 6, 18);
